1f0eeb1592b577ab741b61a61201e55e2542e73c
Bastiaan Teeuwen <bastiaan@mkcl.nl>
2021-04-12 18:47:03 +0200

    add keypad and RFID controls to CLI

--- hbp.py
+++ hbp.py
@@ -1,9 +1,13 @@
 import msgpack
 import socket
+import getpass
 import time
+import serial
 
 host = '127.0.0.1'
 port = 8420
+
+arduino = serial.Serial('/dev/tty.usbmodem14201', 9600, timeout=.1)
 
 class HBP:
     HBP_VERSION =           1
@@ -104,29 +108,130 @@ class HBP:
         else:
             return (reply_type, {msgpack.unpackb(data, raw=False)})
 
+    def replyType(self, reply_type):
+        # TODO put in a list or something but i was lazy
+        if reply_type == self.HBP_REP_LOGIN:
+            return 'HBP_REP_LOGIN'
+        elif reply_type == self.HBP_REP_TERMINATED:
+            return 'HBP_REP_TERMINATED'
+        elif reply_type == self.HBP_REP_INFO:
+            return 'HBP_REP_INFO'
+        elif reply_type == self.HBP_REP_BALANCE:
+            return 'HBP_REP_BALANCE'
+        elif reply_type == self.HBP_REP_TRANSFER:
+            return 'HBP_REP_TRANSFER'
+        elif reply_type == self.HBP_REP_ERROR:
+            return 'HBP_REP_ERROR'
+
     def request(self, request_type, data):
         self.send(request_type, data)
         return self.receive()
 
+def mainmenu():
+    # TODO retrieve name via request
+    name = 'G. Frey'
+
+    print('')
+    print(f'-[ Welkom {name} ]-')
+    print('')
+    print('[1] Geld opnemen')
+    print('[2] Geld doneren')
+    print('[3] Saldo raadplegen')
+    print('[4] Uitloggen')
+
+    choice = '-'
+    while True:
+        data = arduino.readline()[:-2]
+        decoded_data = str(data, 'utf-8')
+
+        if decoded_data[0:1] == 'K':
+            choice = decoded_data[1:]
+            break
+
+    if choice == '1':
+        print('Not Yet Implemented')
+        mainmenu()
+    elif choice == '2':
+        print('Not Yet Implemented')
+        mainmenu()
+    elif choice == '3':
+        print('Not Yet Implemented')
+        mainmenu()
+    elif choice == '4':
+        logout_reply = hbp.request(hbp.HBP_REQ_LOGOUT, [])
+        print(f'Tot ziens {name}!')
+        # TODO we can check the reply, but this is really not needed, as it basically always succeeds
+        login()
+    else:
+        print('Verkeerde keuze')
+        mainmenu()
+
+def login():
+    print('')
+
+    # Get the card ID from the card reader
+    print('Houd uw kaart voor de lezer')
+    card_id = ''
+    while True:
+        data = arduino.readline()[:-2]
+        decoded_data = str(data, 'utf-8')
+
+        if decoded_data[0:1] == 'U':
+            card_id = decoded_data[1:]
+            break
+    print(f'Kaart ID: {card_id}')
+
+    # TODO should get this from the card too
+    account = 'NL35HERB2932749274'
+    print(f'IBAN: {account}')
+
+    # Get the PIN input from the keypad
+    pin = ''
+    print('PIN: ', end='', flush=True)
+    while True:
+        data = arduino.readline()[:-2]
+        decoded_data = str(data, 'utf-8')
+
+        if decoded_data[0:1] == 'K':
+            key = decoded_data[1:]
+            pin += key
+            print(key, end='', flush=True)
+
+            if len(pin) == 4:
+                break
+    print('')
+
+    #pin = getpass.getpass('PIN: ')
+    #pin = input('PIN: ')
+    #try:
+    #    int(pin)
+    #except ValueError:
+    #    print('Foutieve PIN')
+    #    login()
+
+    login_request = [card_id, account, pin]
+    login_reply = hbp.request(hbp.HBP_REQ_LOGIN, login_request)
+
+    reply_type = login_reply[0]
+    if reply_type != hbp.HBP_REP_LOGIN:
+        print(f'Received wrong reply: {hbp.replyType(reply_type)}')
+        login()
+
+    reply_status = list(login_reply[1])[0]
+    if reply_status == hbp.HBP_LOGIN_GRANTED:
+        mainmenu()
+    elif reply_status == hbp.HBP_LOGIN_DENIED:
+        print('Foutieve PIN')
+        login()
+    elif reply_status == hbp.HBP_LOGIN_BLOCKED:
+        print('Kaart geblokkeerd')
+        login()
+    else:
+        print('Erroneous login reply status')
+        login()
+
 print('Copyright (C) 2021 Herbank CLI v1.0')
 hbp = HBP(host, port)
 print(f'Connected to Herbank Server @ {host}:{port}')
-print('')
 
-while True:
-    card_id = input('Card ID: ')
-    try:
-        int(card_id)
-    except ValueError:
-        print("Invalid Card ID")
-        continue
-
-    pin = input('PIN: ')
-    try:
-        int(pin)
-    except ValueError:
-        print("Invalid PIN")
-        continue
-
-    data = [card_id, pin]
-    print(hbp.request(hbp.HBP_REQ_LOGIN, data))
+login()
