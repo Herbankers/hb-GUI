3305e6df2eaf7d505a950416d703e639ea3c3027
Gaurish Arkesteyn <gaurish.ark@gmail.com>
2021-05-06 13:08:18 +0200

    Removed first window: start at scanning card

--- main.py
+++ main.py
@@ -1,65 +1,52 @@
-import sys
-from  PyQt5 import QtWidgets
+import sys, time
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.uic import loadUi
 
-name="Eigenaar"
-
 class Start(QMainWindow):
     def __init__(self):
-        super(Start,self).__init__()
-        loadUi("start.ui",self)
-        self.pushButton_2.clicked.connect(self.gotoLogin1)
+        super(Start, self).__init__()
+        loadUi("start.ui", self)
+        self.startButton.clicked.connect(self.checkCard)
 
-    def gotoLogin1(self):
-        login1= Login1()
-        widget.addWidget(login1)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+    def checkCard(self):
+        c= "test"
+        login = Login(c)
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
-class Login1(QMainWindow):
-    def __init__(self):
-        super(Login1,self).__init__()
-        loadUi("login1.ui",self)
-        self.startButton.clicked.connect(self.gotoLogin2)
-
-    def gotoLogin2(self):
-        cardID = self.cardUID.text()
-        if(len(cardID) == 5):
-            login2= Login2(cardID)
-            widget.addWidget(login2)
-            widget.setCurrentIndex(widget.currentIndex()+1)
-        else:
-            self.label_3.setText("Error: pas incorrect")
-            self.cardUID.setText("")
-            
-class Login2(QMainWindow):
-    def __init__(self,cardID):
-        super(Login2,self).__init__()
+class Login(QMainWindow):
+    def __init__(self, cardID):
+        super(Login, self).__init__()
         self.cardID = cardID
-        loadUi("login2.ui",self)
+        loadUi("login.ui", self)
+        self.pin.setText("")
         self.loginButton.clicked.connect(self.login)
 
     def login(self):
         pincode = self.pin.text()
-        if(len(pincode) == 4):
+        print("login for pair: card: "+ self.cardID+"& pin: "+pincode)
+        if len(pincode) == 4:
             menu = Menu()
             widget.addWidget(menu)
-            widget.setCurrentIndex(widget.currentIndex()+1)
-            print(self.cardID)
-            print(pincode)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+        elif len(pincode) == 0:
+            self.label_3.setText("Error: voer een geldige pincode in")
+            self.pin.setText("")
         else:
-            if(len(pincode) < 4):
+            if len(pincode) < 4:
                 self.label_3.setText("Error: pincode moet 4 cijfers zijn")
                 self.pin.setText("")
-            elif(len(pincode) > 4):
+            elif len(pincode) > 4:
                 self.label_3.setText("Error: pin is maximaal 4 cijfers")
                 self.pin.setText("")
 
+
 class Menu(QMainWindow):
     def __init__(self):
-        super(Menu,self).__init__()
-        loadUi("menu.ui",self)
-        self.label.setText("Welkom "+ name)
+        super(Menu, self).__init__()
+        loadUi("menu.ui", self)
+        self.label.setText("Welkom bij INGrid ")
         self.pushButton.clicked.connect(self.logout)
 
     def logout(self):
@@ -67,8 +54,9 @@ class Menu(QMainWindow):
         widget.addWidget(startWindow)
         widget.setCurrentIndex(0)
 
+
 app = QApplication(sys.argv)
-startWindow=Start()
+startWindow = Start()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(startWindow)
 widget.showMaximized()
