39e48d1ea8a65361dff7398813d560acf1138f3d
Bastiaan Teeuwen <bastiaan@mkcl.nl>
2021-06-10 15:14:00 +0200

    gui: update translations

--- ts/en_US.ts
+++ ts/en_US.ts
@@ -1,255 +1,255 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US" sourcelanguage="nl">
-<context>
+  <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../gui.py" line="848"/>
-        <location filename="../gui.py" line="841"/>
-        <location filename="../gui.py" line="820"/>
-        <location filename="../gui.py" line="175"/>
-        <source>Nog een fijne dag!</source>
-        <translation>Have a nice day!</translation>
+      <location filename="../gui.py" line="848" />
+      <location filename="../gui.py" line="841" />
+      <location filename="../gui.py" line="820" />
+      <location filename="../gui.py" line="175" />
+      <source>Nog een fijne dag!</source>
+      <translation>Have a nice day!</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="297"/>
-        <source>Bedrag   </source>
-        <translation>Amount   </translation>
+      <location filename="../gui.py" line="297" />
+      <source>Bedrag   </source>
+      <translation>Amount   </translation>
     </message>
     <message>
-        <location filename="../gui.py" line="298"/>
-        <source>Datum    </source>
-        <translation>Date     </translation>
+      <location filename="../gui.py" line="298" />
+      <source>Datum    </source>
+      <translation>Date     </translation>
     </message>
     <message>
-        <location filename="../gui.py" line="299"/>
-        <source>Tijd     </source>
-        <translation>Time     </translation>
+      <location filename="../gui.py" line="299" />
+      <source>Tijd     </source>
+      <translation>Time     </translation>
     </message>
     <message>
-        <location filename="../gui.py" line="300"/>
-        <source>Locatie  </source>
-        <translation>Location </translation>
+      <location filename="../gui.py" line="300" />
+      <source>Locatie  </source>
+      <translation>Location </translation>
     </message>
     <message>
-        <location filename="../gui.py" line="411"/>
-        <location filename="../gui.py" line="402"/>
-        <location filename="../gui.py" line="342"/>
-        <source>﹡    Correctie</source>
-        <translation>﹡    Clear</translation>
+      <location filename="../gui.py" line="411" />
+      <location filename="../gui.py" line="402" />
+      <location filename="../gui.py" line="342" />
+      <source>﹡    Correctie</source>
+      <translation>﹡    Clear</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <location filename="../ui/main.ui" line="0"/>
-        <location filename="../gui.py" line="494"/>
-        <location filename="../gui.py" line="480"/>
-        <location filename="../gui.py" line="469"/>
-        <source>﹡    Afbreken</source>
-        <translation>﹡    Abort</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <location filename="../ui/main.ui" line="0" />
+      <location filename="../gui.py" line="494" />
+      <location filename="../gui.py" line="480" />
+      <location filename="../gui.py" line="469" />
+      <source>﹡    Afbreken</source>
+      <translation>﹡    Abort</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="516"/>
-        <source>Welkom</source>
-        <translation>Welcome</translation>
+      <location filename="../gui.py" line="516" />
+      <source>Welkom</source>
+      <translation>Welcome</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="527"/>
-        <location filename="../gui.py" line="518"/>
-        <source>Welkom!</source>
-        <translation>Welcome!</translation>
+      <location filename="../gui.py" line="527" />
+      <location filename="../gui.py" line="518" />
+      <source>Welkom!</source>
+      <translation>Welcome!</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="538"/>
-        <source>Onjuiste PIN</source>
-        <translation>Incorrect PIN</translation>
+      <location filename="../gui.py" line="538" />
+      <source>Onjuiste PIN</source>
+      <translation>Incorrect PIN</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="540"/>
-        <source>Deze kaart is geblokkeerd</source>
-        <translation>This card is blocked</translation>
+      <location filename="../gui.py" line="540" />
+      <source>Deze kaart is geblokkeerd</source>
+      <translation>This card is blocked</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="791"/>
-        <location filename="../gui.py" line="669"/>
-        <location filename="../gui.py" line="542"/>
-        <source>Een interne fout is opgetreden</source>
-        <translation>An internal error has occurred</translation>
+      <location filename="../gui.py" line="791" />
+      <location filename="../gui.py" line="669" />
+      <location filename="../gui.py" line="542" />
+      <source>Een interne fout is opgetreden</source>
+      <translation>An internal error has occurred</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="635"/>
-        <source>Een moment geduld...</source>
-        <translation>One moment please...</translation>
+      <location filename="../gui.py" line="635" />
+      <source>Een moment geduld...</source>
+      <translation>One moment please...</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="655"/>
-        <source>Geld uitgave niet beschikbaar, wel afgeschreven 😈</source>
-        <translation>Cash dispenser not available, credited anyways 😈</translation>
+      <location filename="../gui.py" line="655" />
+      <source>Geld uitgave niet beschikbaar, wel afgeschreven 😈</source>
+      <translation>Cash dispenser not available, credited anyways 😈</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="781"/>
-        <location filename="../gui.py" line="659"/>
-        <source>Uw saldo is ontoereikend</source>
-        <translation>Your balance is insufficient</translation>
+      <location filename="../gui.py" line="781" />
+      <location filename="../gui.py" line="659" />
+      <source>Uw saldo is ontoereikend</source>
+      <translation>Your balance is insufficient</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="775"/>
-        <source>Bedankt voor uw donatie!</source>
-        <translation>Thank you for your donation!</translation>
+      <location filename="../gui.py" line="775" />
+      <source>Bedankt voor uw donatie!</source>
+      <translation>Thank you for your donation!</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="818"/>
-        <source>Een bon is helaas niet beschikbaar</source>
-        <translation>A receipt is not available unfortunately</translation>
+      <location filename="../gui.py" line="818" />
+      <source>Een bon is helaas niet beschikbaar</source>
+      <translation>A receipt is not available unfortunately</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="830"/>
-        <source>Wilt u een bon?</source>
-        <translation>Would you like a receipt?</translation>
+      <location filename="../gui.py" line="830" />
+      <source>Wilt u een bon?</source>
+      <translation>Would you like a receipt?</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="831"/>
-        <source>﹡    Nee</source>
-        <translation>﹡    No</translation>
+      <location filename="../gui.py" line="831" />
+      <source>﹡    Nee</source>
+      <translation>﹡    No</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="832"/>
-        <source>Ja    #</source>
-        <translation>Yes    #</translation>
+      <location filename="../gui.py" line="832" />
+      <source>Ja    #</source>
+      <translation>Yes    #</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>INGrid</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <location filename="../ui/main.ui" line="0" />
+      <source>INGrid</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Houdt uw kaart voor de lezer</source>
-        <translation>Hold your card in front of the reader</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Houdt uw kaart voor de lezer</source>
+      <translation>Hold your card in front of the reader</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>1</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>1</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>2</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>2</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>3</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>3</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Voer uw PIN in</source>
-        <translation>Please enter your PIN</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Voer uw PIN in</source>
+      <translation>Please enter your PIN</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>1    Geld opnemen</source>
-        <translation>1    Cash withdrawal</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>1    Geld opnemen</source>
+      <translation>1    Cash withdrawal</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>4    Geld doneren</source>
-        <translation>4    Donate</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>4    Geld doneren</source>
+      <translation>4    Donate</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>7    Saldo opvragen</source>
-        <translation>7    Balance inquiry</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>7    Saldo opvragen</source>
+      <translation>7    Balance inquiry</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>€70 opnemen    6</source>
-        <translation>Withdraw €70    6</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>€70 opnemen    6</source>
+      <translation>Withdraw €70    6</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Uitloggen    #</source>
-        <translation>Logout    #</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Uitloggen    #</source>
+      <translation>Logout    #</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Selecteer het op te nemen bedrag</source>
-        <translation>Select the amount to withdraw</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Selecteer het op te nemen bedrag</source>
+      <translation>Select the amount to withdraw</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>1    10 EUR</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>1    10 EUR</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>4    20 EUR</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>4    20 EUR</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>50 EUR    3</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>50 EUR    3</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>100 EUR    6</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>100 EUR    6</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Eigen invoer    #</source>
-        <translation>Custom amount    #</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Eigen invoer    #</source>
+      <translation>Custom amount    #</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Voer het op te nemen bedrag in</source>
-        <translation>Enter the amount to withdraw</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Voer het op te nemen bedrag in</source>
+      <translation>Enter the amount to withdraw</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>    EUR</source>
-        <translation></translation>
+      <location filename="../ui/main.ui" line="0" />
+      <location filename="../ui/main.ui" line="0" />
+      <source>    EUR</source>
+      <translation />
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Akkoord    #</source>
-        <translation>Accept    #</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <location filename="../ui/main.ui" line="0" />
+      <source>Akkoord    #</source>
+      <translation>Accept    #</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Selecteer uw biljetten</source>
-        <translation>Select your bill mix</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Selecteer uw biljetten</source>
+      <translation>Select your bill mix</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Voer het te doneren bedrag in</source>
-        <translation>Please enter the amount to withdraw</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Voer het te doneren bedrag in</source>
+      <translation>Please enter the amount to withdraw</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Uw saldo bedraagt</source>
-        <translation>Your balance is</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Uw saldo bedraagt</source>
+      <translation>Your balance is</translation>
     </message>
     <message>
-        <location filename="../ui/main.ui" line="0"/>
-        <source>Terugkeren    #</source>
-        <translation>Return    #</translation>
+      <location filename="../ui/main.ui" line="0" />
+      <source>Terugkeren    #</source>
+      <translation>Return    #</translation>
     </message>
     <message>
-        <source>Geld dispenser niet beschikbaar, wel afgeschreven 😈</source>
-        <translation type="vanished">Cash dispenser not available, credited anyways 😈</translation>
+      <source>Geld dispenser niet beschikbaar, wel afgeschreven 😈</source>
+      <translation type="vanished">Cash dispenser not available, credited anyways 😈</translation>
     </message>
     <message>
-        <source>Biljetkeuze    9</source>
-        <translation type="vanished">Select Bills    9</translation>
+      <source>Biljetkeuze    9</source>
+      <translation type="vanished">Select Bills    9</translation>
     </message>
     <message>
-        <source>€70 opnemen    3</source>
-        <translation type="vanished">Withdraw €70    3</translation>
+      <source>€70 opnemen    3</source>
+      <translation type="vanished">Withdraw €70    3</translation>
     </message>
-</context>
+  </context>
 </TS>
